66103a448f3bc52faf00166ead9e505db2e35878
Thomas Morris <tomchrismorris@yahoo.com>
2022-09-23 14:32:19 -0400

    adds user list and detail endpoints

--- stickers/urls.py
+++ stickers/urls.py
@@ -6,6 +6,8 @@ from stickers import views
 urlpatterns = [
     path('stickers/', views.StickerList.as_view(), name="all-stickers"),
     path('stickers/<int:pk>/', views.StickerDetail.as_view(), name='sticker-detail'),
+    path('users/', views.UserList.as_view(), name="all-users"),
+    path('profile/<int:pk>/', views.UserDetail.as_view(), name='profile-detail'),
     path('', views.api_root),
 ]
 
