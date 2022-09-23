75bee684939e381c9d1a2a62ed6d55e7d567243e
heathersmith <moellio.heather@gmail.com>
2022-09-23 12:47:53 -0400

    adds urls choices for patterns

--- stickers/models.py
+++ stickers/models.py
@@ -5,23 +5,26 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    bio = models.TextField(max_length=200, help_text='Enter a bio', blank=True, null=True)
+    bio = models.TextField(
+        max_length=200, help_text='Enter a bio', blank=True, null=True)
     avatar = models.URLField(max_length=100, blank=True, null=True)
     display_name = models.CharField(
         max_length=25,
         help_text='Enter the name you want to display',
         blank=True,
         null=True
-        )
+    )
 
     def __str__(self):
         return f'username={self.username}: displayname={self.display_name}'
 
 
 class Follow(models.Model):
-    following_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='following', blank=True, null=True)
-    followed_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='followed_by', blank=True, null=True)
-    
+    following_user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='following', blank=True, null=True)
+    followed_user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='followed_by', blank=True, null=True)
+
     def __str__(self):
         return f'Follower:{self.follower.username} Following:{self.following.username}'
 
@@ -41,8 +44,22 @@ COLOR_CHOICES = [
 ]
 
 PATTERN_CHOICES = [
-    ('argyle', 'Argyle'),
-    ('stripes', 'Stripes'),
+    ('https://www.transparenttextures.com/patterns/black-thread.png',
+     'black-thread'),
+    ('https://www.transparenttextures.com/patterns/bo-play.png',
+        'bo-play'),
+    ('https://www.transparenttextures.com/patterns/brick-wall.png',
+        'brick-wall'),
+    ('https://www.transparenttextures.com/patterns/clean-textile.png',
+        'clean-textile'),
+    ('https://www.transparenttextures.com/patterns/crisp-paper-ruffles.png',
+        'crisp-paper-ruffles'),
+    ('https://www.transparenttextures.com/patterns/cutcube.png',
+        'cutcube'),
+    ('https://www.transparenttextures.com/patterns/diagmonds-light.png',
+        'diagmonds-light'),
+    ('https://www.transparenttextures.com/patterns/dimension.png',
+        'dimensions')
 ]
 
 FONT_CHOICES = [
