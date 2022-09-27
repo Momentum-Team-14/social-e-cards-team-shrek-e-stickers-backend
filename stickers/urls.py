26af806b7707b0aadc65459c5e9176f9e9bcdad5
Thomas Morris <tomchrismorris@yahoo.com>
2022-09-27 17:59:34 -0400

    adds creator id to serializers, adds list of followers stickers

--- stickers/urls.py
+++ stickers/urls.py
@@ -7,14 +7,17 @@ urlpatterns = [
     path('stickers/', views.StickerList.as_view(), name="all-stickers"),
     path('stickers/<int:pk>/', views.StickerDetail.as_view(), name='sticker-detail'),
     path('users/', views.UserList.as_view(), name="all-users"),
+    path('myprofile/', views.UserProfile.as_view(), name='my-profile'),
     path('profile/<int:pk>/', views.UserDetail.as_view(), name='profile-detail'),
     path('user/<int:pk>/stickers/',
          views.UserStickerList.as_view(), name='user-sticker'),
-    # may need to use the userpk inbetween
+    path('mystickers/', views.UserStickerList.as_view(), name='my-stickers'),
     path('user/<int:pk>/follow/', views.FollowCreate.as_view(), name='follow-user'),
+    path('user/<int:pk>/unfollow/', views.UnFollowDestroy.as_view(), name='unfollow-user'),
     path('following/', views.FollowingList.as_view(), name="following-users"),
     path('followed-by/', views.FollowedList.as_view(), name="followed-by-users"),
     path('', views.api_root),
+    path('stickers/following/', views.FollowListStickers.as_view(), name="follow-stickers"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
